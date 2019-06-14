d4be3b8e50fb6e323abe0e975920a92a13b31e20
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-14 12:13:00 +0300

    imported modules and created the user class

---
+++ password.py
@@ -0,0 +1,19 @@
+import string
+import random
+import pyper clip
+
+
+#Global variable
+global user-list
+
+
+class User:
+ '''
+ A class to create user accounts and save them
+ 
+ '''
+ #global variable user_list
+ 
+ user-list =[]
+ 
+ def __init__(self, firstname, lastname, password):
