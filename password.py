4820501ff6c9aae22e5e309c725b41af01131d54
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-14 12:22:38 +0300

    created the user-list function and the intances

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ import string
 import random
 import pyper clip
 
-
+#Class variables
 #Global variable
 global user-list
 
@@ -17,3 +17,10 @@ class User:
  user-list =[]
  
  def __init__(self, firstname, lastname, password):
+    '''
+    method to define what property will be in each user object
+    '''
+    #intance variables
+    self.first_name = first-name
+    self.last_name = last_name
+    self.password = password
