4d45e0dd003da8bb1674b2a4af614d3f3cbafc3b
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-15 14:12:09 +0300

    initialized the test function to check if the creation of the user intances are done correctly

--- test-password.py
+++ test-password.py
@@ -4,13 +4,24 @@ from password import User
 
 
 class TestUser(unittest.TestCase):
-    
-'''
-This defines the TestCases for the user test behavior
-Arg:
-    unittest TestCase: helps in creating test cases.
-    
-'''
+    '''
+    Test class that define the test cases for user class behavior
+    Arg:
+    unittest.TestCase: helps in creating test cases.
+    '''
+    def setUp(self):
+        '''
+        Function to create a user account before each test
+        '''
+        self.new_user = User('Viv','Kimani', 'her1234')
+        
+    def test__init__(self):
+        '''
+        Test to check if the creation of user instances is done properly
+        '''
+        self.assertEqual(self.new_user.first_name, 'Viv')
+        self.assertEqual(self.new_user.last_name, 'Kimani')
+        self.assertEqual(self.new_user.password, 'her1234')
 
     
     
