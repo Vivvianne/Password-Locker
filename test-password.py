de04a0b17d0cd8f49d77295e11ba7b983f8f94b0
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-15 14:42:40 +0300

    Function to test whether the login in function check_user works as expected in test_password.py

--- test-password.py
+++ test-password.py
@@ -31,5 +31,29 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(User.users_list),1)
     
     
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines the test cases of the behaviors for the credential class.
+    Args:
+        unittest.TestCases: helps in creating test cases.
+    '''
+    
+    def test_check_user(self):
+        '''
+        Function to test whether the login in function check_user works as expected.
+        '''
+        self.new_user = User('Viv','Kimani','her1234')
+        self.new_user.save_user()
+        user1 = User('Viv','Kimani','her1234')
+        user1.save_user()
+        
+        for user in User.users_list:
+            if user.first_name = user1.first_name and user.password = user1.password:
+                current_user = user.first_name
+        return current_user
+    
+        self.assertEqual(current_user, Credential.check_user(user1.password,user1.first_name))
+    
+    
     
     
